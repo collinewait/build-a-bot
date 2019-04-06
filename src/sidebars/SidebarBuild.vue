8c8afb8f8545bd6e95a46c7d78b1ef52626d076f
collinewait <collinewait17@gmail.com>
2019-04-06 21:17:20 +0300

    add sample sidebars and use named views

---
+++ src/sidebars/SidebarBuild.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="sidebar">
+    Buld Sidebar
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Build',
+};
+</script>
+
+<style>
+.sidebar {
+  font-size: 50px;
+  transform: rotate(90deg);
+  color: green;
+}
+</style>
