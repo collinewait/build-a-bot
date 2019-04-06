2f77f1121912cadfa7f7e85aa4507e8b41eceddd
collinewait <collinewait17@gmail.com>
2019-04-06 18:38:24 +0300

    use updated life cycle method hook to emite events

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -44,6 +44,9 @@ export default {
   created() {
     this.emitSelectedPart();
   },
+  updated() {
+    this.emitSelectedPart();
+  },
   methods: {
     emitSelectedPart() {
       this.$emit('partSelected', this.selectedPart);
@@ -53,14 +56,12 @@ export default {
         this.selectedPartIndex,
         this.parts.length,
       );
-      this.emitSelectedPart();
     },
     selectPreviousPart() {
       this.selectedPartIndex = getPreviousValidIndex(
         this.selectedPartIndex,
         this.parts.length,
       );
-      this.emitSelectedPart();
     },
 
   },
