c20368197f6a859e67e62f2259c00b5fb4ccc81c
collinewait <collinewait17@gmail.com>
2019-04-06 19:02:50 +0300

    hide and show preview by injecting content into child component with slots

---
+++ src/shared/CollapsibleSection.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div class="header">
+      <span v-if="open" @click="open = !open">&#x25B2;
+         Hide preview
+      </span>
+      <span v-if="!open" @click="open = !open">&#x25BC;
+        Show preview
+      </span>
+    </div>
+    <!-- Toggleable-->
+    <slot v-if="open">
+      <div>DefaultContent</div>
+    </slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CollapsibleSection',
+  data() {
+    return { open: true };
+  },
+};
+</script>
+
+<style scoped>
+.header {
+  background-color: #bbb;
+  padding: 3px;
+  cursor: pointer;
+}
+</style>
