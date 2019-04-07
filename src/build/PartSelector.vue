cbcfa6f185701a2b3bef99236138153738cba29b
collinewait <collinewait17@gmail.com>
2019-04-07 23:55:58 +0300

    use an object to pass data to the directive

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -11,7 +11,9 @@
     </router-link>
     <button @click="selectPreviousPart()" class="prev-selector"></button>
     <button @click="selectNextPart()" class="next-selector"></button>
-    <span v-pin.position.top.right class="sale" v-show="selectedPart.onSale">Sale!</span>
+    <span v-pin="{ bottom: '5px', right: '5px' }" class="sale" v-show="selectedPart.onSale">
+      Sale!
+    </span>
   </div>
 </template>
 
