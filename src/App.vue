18024f31bd7306f6b2b25a33cd55caba0c771e28
collinewait <collinewait17@gmail.com>
2019-04-07 17:07:27 +0300

    display number of items in the cart

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,9 @@
             <router-link class="nav-link" to="/cart" exact>
             Cart
             </router-link>
+            <div class="cart-items">
+              {{cart.length}}
+            </div>
           </li>
         </ul>
       </nav>
@@ -38,6 +41,11 @@
 
 export default {
   name: 'app',
+  computed: {
+    cart() {
+      return this.$store.state.cart;
+    },
+  },
 };
 </script>
 
@@ -99,5 +107,16 @@ ul {
   background-color: #aaa;
   width: 100px;
   min-height: 300px;
+}
+.cart-items {
+  position: absolute;
+  top: -5px;
+  right: -9px;
+  font-size: 18px;
+  widows: 20px;
+  text-align: center;
+  display: inline-block;
+  border-radius: 100px;
+  background-color: mediumseagreen;
 }
 </style>
