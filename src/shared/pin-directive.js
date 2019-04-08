b99dfb439cf7db426f0d0ef7f904a8bee0e90b12
collinewait <collinewait17@gmail.com>
2019-04-08 09:39:46 +0300

    use directive life cycle hooks

--- src/shared/pin-directive.js
+++ src/shared/pin-directive.js
@@ -1,8 +1,15 @@
+function applyStle(element, binding) {
+  Object.keys(binding.value).forEach((position) => {
+    element.style[position] = binding.value[position];
+  });
+  element.style.position = 'absolute';
+}
+
 export default {
   bind: (element, binding) => {
-    Object.keys(binding.value).forEach((position) => {
-      element.style[position] = binding.value[position];
-    });
-    element.style.position = 'absolute';
+    applyStle(element, binding);
+  },
+  update: (element, binding) => {
+    applyStle(element, binding);
   },
 };
