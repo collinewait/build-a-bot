d6306123d9a5a313ede670ce5dd6519397e0acb0
collinewait <collinewait17@gmail.com>
2019-04-08 09:44:01 +0300

    use shorthand notation in directives

--- src/shared/pin-directive.js
+++ src/shared/pin-directive.js
@@ -1,15 +1,6 @@
-function applyStle(element, binding) {
+export default function (element, binding) {
   Object.keys(binding.value).forEach((position) => {
     element.style[position] = binding.value[position];
   });
   element.style.position = 'absolute';
 }
-
-export default {
-  bind: (element, binding) => {
-    applyStle(element, binding);
-  },
-  update: (element, binding) => {
-    applyStle(element, binding);
-  },
-};
